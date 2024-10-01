15878845f99ccd756eb2ad0229fe6b5bca87841d
tiway <stilon670@gmail.com>
2024-10-01 15:05:52 +0300

    Added count products in category and photo for product to admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Category, Product, Gallery
+
+
+admin.site.register(Gallery)
 
 
 class GalleryInline(admin.TabularInline):
@@ -10,14 +15,22 @@ class GalleryInline(admin.TabularInline):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["title", "parent"]
+    list_display = ["title", "parent", "get_category_count"]
     prepopulated_fields = {"slug": ("title",)}
+
+    def get_category_count(self, obj):
+        if obj.products:
+            return len(obj.products.all())
+        return "0"
+
+    get_category_count.short_description = "Кількість товарів"
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = [
         "pk",
+        "get_photo",
         "title",
         "category",
         "quantity",
@@ -31,3 +44,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ["title", "price"]
     list_display_links = ["pk", "title"]
     inlines = (GalleryInline,)
+
+    def get_photo(self, obj):
+        if obj.images.all():
+            return mark_safe(
+                f"<img src='{obj.images.all()[0].image.url}' width='75'>"
+            )
+        else:
+            return "-"
